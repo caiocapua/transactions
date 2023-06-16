68b70ed22cfef9da0b186ac62ec715daccb3b9be
caiocapua <caiocapua@hotmail.com>
2023-06-16 17:51:13 -0300

    update

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,9 @@ import { app } from './app'
 
 app
   .listen({
+    host: '0.0.0.0',
     port: 3000,
   })
   .then(() => {
-    console.log('HTTP Server Running')
+    console.log('HTTP Server Running! 🚀')
   })
